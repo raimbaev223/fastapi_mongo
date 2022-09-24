b3992d1406677d40506fc2db76b0fcc0c1db1b00
E.Raimbaev <e.raimbaev@houseofapps.ru>
2022-09-24 20:51:49 +0600

    added crud and routes for user

---
+++ app/server/database.py
@@ -0,0 +1,105 @@
+import motor.motor_asyncio
+
+
+MONGO_DETAILS = "mongodb://localhost:27017"
+client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
+database = client.chatFastApi
+users_collection = database.get_collection("users_collection")
+# helpers
+
+
+def user_helper(user) -> dict:
+    return {
+        "id": str(user["_id"]),
+        "email": str(user["email"]),
+        "userId": user["userId"],
+        "isDeleted": user["isDeleted"],
+    }
+
+
+# Retrieve all users present in the database
+async def retrieve_users():
+    users = []
+    async for user in users_collection.find({"isDeleted": False}):
+        users.append(user_helper(user))
+    return users
+
+
+# Add a new user into to the database
+async def add_user(user_data: dict) -> dict:
+    old_user = await users_collection.find_one(
+        {
+            "userId": user_data["userId"],
+            "email": user_data["email"],
+            "isDeleted": False,
+        })
+    if old_user:
+        return "User Already added"
+    user = await users_collection.insert_one(user_data)
+    new_user = await users_collection.find_one(
+        {
+            "_id": user.inserted_id,
+            "isDeleted": False,
+        }
+    )
+    return user_helper(new_user)
+
+
+# Retrieve a user with a matching ID
+async def retrieve_user(id: int) -> dict:
+    user = await users_collection.find_one(
+        {
+            "userId": int(id),
+            "isDeleted": False,
+        }
+    )
+    if user:
+        return user_helper(user)
+
+
+# Update a user with a matching ID
+async def update_user(id: int, data: dict):
+    # Return false if an empty request body is sent.
+    if len(data) < 1:
+        return False
+    user = await users_collection.find_one(
+        {
+            "userId": int(id),
+            "isDeleted": False,
+        }
+    )
+    if user:
+        updated_user = await users_collection.update_one(
+            {
+                "userId": int(id),
+                "isDeleted": False,
+            }, {
+                "$set": data,
+            }
+        )
+        if updated_user:
+            return True
+        return False
+
+
+# Delete a user from the database
+async def delete_user(id: int):
+    user = await users_collection.find_one(
+        {
+            "userId": int(id),
+            "isDeleted": False,
+        }
+    )
+    if user:
+        await users_collection.update_one(
+            {
+                "userId": int(id),
+                "isDeleted": False,
+            },
+            {
+                "$set": {
+                    "isDeleted": True,
+                }
+            }
+        )
+        return True
---
+++ app/server/models/user.py
@@ -0,0 +1,45 @@
+from typing import Optional
+
+from pydantic import BaseModel, EmailStr, Field
+
+
+class UserSchema(BaseModel):
+    email: EmailStr = Field(...)
+    userId: int = Field(...)
+    isDeleted: bool = Field(default=False)
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "jdoe@x.ex",
+                "userId": 20012,
+                "isDeleted": False,
+            }
+        }
+
+
+class UpdateUserModel(BaseModel):
+    email: Optional[EmailStr]
+    userId: Optional[int]
+    isDeleted: Optional[bool]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "jdoe@x.ex",
+                "userId": 20012,
+                "isDeleted": False,
+            }
+        }
+
+
+def ResponseModel(data, message):
+    return {
+        "data": [data],
+        "code": 200,
+        "message": message,
+    }
+
+
+def ErrorResponseModel(error, code, message):
+    return {"error": error, "code": code, "message": message}
